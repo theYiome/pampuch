215a2639138bfd6525dd0d28acb80c5c2c8ade5a
Jakub Salamon <33131677+Jacobsky98@users.noreply.github.com>
2020-05-13 22:52:38 +0200

    Add Sqlalchemy and some new api

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io
 import server_utils as utils
 
 app = flask.Flask(__name__, static_url_path="", static_folder="static")
-
+driver = None
 
 @app.route("/")
 def redirect_to_menu():
@@ -19,14 +19,20 @@ def redirect_to_apidoc():
 
 @app.route("/api/images")
 def return_image_ids():
-    connection = utils.sql_connection()
-    return utils.sql_get_images(connection)
-
+    return driver.select_images()
 
 @app.route("/api/image/<string:img_id>")
 def return_image(img_id):
-    connection = utils.sql_connection()
-    return utils.sql_get_images(connection, img_id)
+    return driver.select_images(img_id = img_id)
+
+@app.route("/api/images/label/<string:label>")
+def return_images_by_label(label):
+    return driver.select_images(img_label = label)
+
+@app.route("/api/images/labels")
+def return_labels():
+    return driver.select_labels()
+
 
 
 @app.route("/api/save", methods=['POST'])
@@ -36,7 +42,6 @@ def save_image():
     image = Image.open(io.BytesIO(bin_image))
     # with open("data_sent_by_post.png", "wb") as f:
     #     f.write(bin_image)
-    connection = utils.sql_connection()
     for rect in data["rects"]:
         label = rect["label"]
         left = int(rect["x"])
@@ -45,7 +50,10 @@ def save_image():
         top = int(rect["y"])
         labeled_image = image.crop((left, top, right, bottom))
         labeled_image = labeled_image.resize((32, 32))
-        utils.sql_insert_image(connection, labeled_image, label)
+        image_bytes = io.BytesIO()
+        labeled_image.save(image_bytes, format='PNG')
+        image_bytes = image_bytes.getvalue()
+        driver.insert_image(image_bytes, label)
     return "Save image with its label in database\n" + json.dumps(data, indent=4)
 
 
@@ -70,6 +78,6 @@ def recognize_image():
 
 
 if __name__ == "__main__":
-    connection = utils.sql_connection()
-    utils.sql_create_table(connection)
+    driver = utils.SQL_driver()
+    driver.create_tables()
     app.run(debug=True)
